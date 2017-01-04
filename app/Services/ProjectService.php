d505b3db82abb1ec6ccf715fd1d90641ddb22b4a
Danilo Dorotheu <danilo.dorotheu@live.com>
2017-01-04 23:16:57 +0000

    - Corrigindo os Services de Client, Project e ProjectNote, extendendo o AbstractService para abstrair o código;

--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -4,83 +4,16 @@ namespace App\Services;
 
 use App\Repositories\ProjectRepository;
 use App\Validators\ProjectValidator;
-use Prettus\Validator\Exceptions\ValidatorException;
 
-class ProjectService
+class ProjectService extends AbstractService
 {
-
-	/**
-     * @var ClientRepository
-     */
-    private $repository;
-
-    /**
-     * @var ClientValidator
-     */
-    private $validator;
 
 	/**
      * @author Danilo Dorotheu <danilo.dorotheu@live.com>
      */
     public function __construct(ProjectRepository $repository, ProjectValidator $validator)
     {
-        $this->repository = $repository;
-        $this->validator  = $validator;
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  array $data
-     * @return array
-     */
-    public function create(array $data)
-    {
-        try {
-
-            $this->validator->with($data)->passesOrFail();
-            return $this->repository->create($data);
-
-        }catch(ValidatorException $e){
-            return [
-                'error'   => true,
-                'message' => $e->getMessageBack()
-            ];
-        }
-
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  array  $data
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(array $data, $id)
-    {
-        try {
-
-            $this->validator->with($data)->passesOrFail();
-            return $this->repository->update($data, $id);
-
-        }catch(ValidatorException $e){
-            return [
-                'error'   => true,
-                'message' => $e->getMessageBag()
-            ];
-        }
-        
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     */
-    public function destroy($id)
-    {
-        $this->repository->delete($id);
+        parent::__construct($repository, $validator, 'Projeto');
     }
 
 }
